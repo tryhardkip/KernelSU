f27c085a3de9827ebaa295ab60908bb7f168598e
manipvlator <mr.morat14@gmail.com>
2026-08-30 05:42:40 +0700

    kernel: implement susfs
    Signed-off-by: manipvlator <mr.morat14@gmail.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -154,6 +154,10 @@
 #include "hook/kp_ksud.c"
 #endif
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif // #ifdef CONFIG_KSU_SUSFS
+
 // track backports and other quirks here
 // ref: kernel_compat.c, Makefile
 // yes looks nasty
@@ -263,6 +267,10 @@ static int __init kernelsu_init(void)
 	ksu_allowlist_init();
 
 	ksu_throne_tracker_init();
+
+#ifdef CONFIG_KSU_SUSFS
+    susfs_init();
+#endif // #ifdef CONFIG_KSU_SUSFS
 
 	ksu_ksud_init();
 
